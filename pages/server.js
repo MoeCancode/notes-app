667b5dcdfa79f4408c1d0823e4692058b3ccacfc
M A Razvi <abuzarflw@gmail.com>
2022-04-27 18:40:09 -0700

    Set path for button on main page to redirect to notes page

--- pages/server.js
+++ pages/server.js
@@ -5,17 +5,25 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.listen(port);
-console .log(`Server started at: http://localhost:${port}`);
+console.log(`Server started at: http://localhost:${port}`);
 
 //Set index.html as root page
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-})
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 //Set notes.html as notes page
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "notes.html"));
-})
-//Set any other ur; to redirect to index.html page
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+//
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "db.json"));
+});
+
+//Set any other url to redirect to index.html page
 app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-})
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
